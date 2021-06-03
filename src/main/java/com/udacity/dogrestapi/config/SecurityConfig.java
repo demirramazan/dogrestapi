b1d655ba7a8429a235c6f1240b96fc987679f713
ramazan.demir <ramazan.demir9@icisleri.gov.tr>
2021-06-03 15:27:10 +0300

    spring security config added

---
+++ src/main/java/com/udacity/dogrestapi/config/SecurityConfig.java
@@ -0,0 +1,35 @@
+package com.udacity.dogrestapi.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable()
+                .authorizeRequests().anyRequest()
+                .authenticated()
+                .and().httpBasic();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("admin")
+                .password(enconder().encode("password"))
+                .roles("USER");
+    }
+
+    @Bean
+    public PasswordEncoder enconder() {
+        return new BCryptPasswordEncoder();
+    }
+}
